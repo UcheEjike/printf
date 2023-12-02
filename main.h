ed41563f13211aad8f9fbc85728fbb5a2476128c
Joseph Uche <ucheejike161@gmail.com>
2023-12-02 21:43:39 +0100

    Updated the printf to handle conversion specifiers 'd' and 'i'

--- main.h
+++ main.h
@@ -6,6 +6,8 @@
 #define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
+void process_format(const char *format, va_list args, char buffer[], int *buff_ind, int *printed_chars);
 void flush_buffer(char buffer[], int *buff_ind);
+void int_to_str(int value, char buffer[], int *buff_ind);
 
 #endif /* MAIN_H */
