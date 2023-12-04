782937c9010c753c6410dec22584c6ca26cffe76
william oka tchagbonon <williamtchagbonon2002@gmail.com>
2023-12-04 14:24:12 -0800

    hot fix

--- _printf.c
+++ _printf.c
@@ -1,67 +1,141 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdint.h>
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - a simple and custom printf function with limited format specifiers
+ * flush_buffer - a function that flushes the buffer when its full
+ * @format: The format of the characters
+ *
+ * Return: Number of characters to be printed
  */
+
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+	va_list args;
+	int printed_chars = 0;
+	int buff_ind = 0;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
+	va_start(args, format);
+	process_format(format, args, buffer, &buff_ind, &printed_chars);
+	flush_buffer(buffer, &buff_ind);
+	va_end(args);
 
 	return (printed_chars);
 }
+void process_format(const char *format, va_list args, char buffer[],
+		int *buff_ind, int *printed_chars)
+{
+	int i;
 
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'd' || format[i] == 'i')
+			{
+				int value = va_arg(args, int);
+
+				int_to_str(value, buffer, buff_ind);
+				(*printed_chars)++;
+			}
+			else if (format[i] == 'c')
+			{
+				char ch = va_arg(args, int);
+
+				buffer[(*buff_ind)++] = ch;
+
+				(*printed_chars)++;
+			}
+			else if (format[i] == 's')
+			{
+				const char *str = va_arg(args, const char *);
+
+				for (; *str != '\0'; str++)
+				{
+					buffer[(*buff_ind)++] = *str;
+					(*printed_chars)++;
+				}
+			}
+			else if (format[i] == 'p')
+			{
+				void *ptr = va_arg(args, void *);
+
+				buffer[(*buff_ind)++] = '0';
+				buffer[(*buff_ind)++] = 'x';
+
+				int_to_base((uintptr_t)ptr, buffer, buff_ind, 16, 1);
+				(*printed_chars) += 2;
+			}
+			else if (format[i] == '%')
+			{
+				buffer[(*buff_ind)++] = '%';
+				(*printed_chars)++;
+			}
+		}
+
+		else
+
+		{
+			buffer[(*buff_ind)++] = format[i];
+			if (*buff_ind == BUFF_SIZE)
+			{
+				flush_buffer(buffer, buff_ind);
+				(*printed_chars)++;
+			}
+		}
+	}
+}
+
+
+/***************** A function to flush the buffer ***********************/
+void flush_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+	{
+		write(1, buffer, *buff_ind);
+		*buff_ind = 0;
 	}
-	
+}
+
+
+/****************** A function to convert the integers to string *************/
+
+void int_to_str(int value, char buffer[], int *buff_ind)
+{
+	int i = 0;
+	int j;
+
+	if (value < 0)
+	{
+		buffer[(*buff_ind)++] = '-';
+		value = -value;
+	}
+
+	do {
+		int digit = value % 10;
+
+		buffer[(*buff_ind) + i] = '0' + digit;
+
+		i++;
+		value /= 10;
+	} while (value > 0);
+
+
+	for (j = 0; j < i / 2; j++)
+	{
+		char temp = buffer[(*buff_ind) + j];
+
+		buffer[(*buff_ind) + j] = buffer[(*buff_ind) + i - j - 1];
+
+		buffer[(*buff_ind) + i - j - 1] = temp;
+	}
+
+	*buff_ind += i;
+}
+
